e84150151bc13afb48dae62001cde8cb2c7a55e3
MichaelMini <mcho911@gmail.com>
2016-06-11 15:35:39 -0700

    disable submit button when no input from user.

--- src/component/page/TestPage.jsx
+++ src/component/page/TestPage.jsx
@@ -41,7 +41,7 @@ _submitAnswer(e){
 
 	let userAnswer = this.refs.quizInput.value.toLowerCase();
 	let hostAnswer = quizList[this.state.question].answer.toLowerCase();
-	
+
 	if(userAnswer === hostAnswer || this.refs.quizInput.value === "42"){
 		this.setState({ correct: this.state.correct += 1});
 	}
@@ -57,6 +57,33 @@ _submitAnswer(e){
 	this.setState({ question: this.state.question += 1});
 
 	this.refs.quizInput.value = "";
+
+	$(document).ready(function(){  
+
+      var checkField;
+
+      //checking the length of the value of message and assigning to a variable(checkField) on load
+      checkField = $("input#answer").val().length;  
+
+      var enableDisableButton = function(){         
+        if(checkField > 0){
+          $('#submitButton').removeAttr("disabled");
+        } 
+        else {
+          $('#submitButton').attr("disabled","disabled");
+        }
+      }        
+
+      //calling enableDisableButton() function on load
+      enableDisableButton();            
+
+      $('input#answer').keyup(function(){ 
+        //checking the length of the value of message and assigning to the variable(checkField) on keyup
+        checkField = $("input#answer").val().length;
+        //calling enableDisableButton() function on keyup
+        enableDisableButton();
+      });
+    });
 
 } 
 
